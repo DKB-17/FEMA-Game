de3850549ff8b3aae7217d2aa233ddede883efdf
diego <136401880+DKB-17@users.noreply.github.com>
2024-05-27 22:44:04 -0300

    feat: mudanca de IDE

--- Bomberman.cpp
+++ Bomberman.cpp
@@ -5,18 +5,18 @@
 
 using namespace std;
 
-ALLEGRO_DISPLAY *janela = NULL;
-ALLEGRO_EVENT_QUEUE *fEventos = NULL;
+ALLEGRO_DISPLAY* janela = NULL;
+ALLEGRO_EVENT_QUEUE* fEventos = NULL;
 ALLEGRO_EVENT ev;
 
-ALLEGRO_BITMAP *chao =  NULL;
-ALLEGRO_BITMAP *parede =  NULL;
-ALLEGRO_BITMAP *porta =  NULL;
-ALLEGRO_BITMAP *centro_lousa =  NULL;
-ALLEGRO_BITMAP *esquerda_lousa =  NULL;
-ALLEGRO_BITMAP *direita_lousa =  NULL;
+ALLEGRO_BITMAP* chao = NULL;
+ALLEGRO_BITMAP* parede = NULL;
+ALLEGRO_BITMAP* porta = NULL;
+ALLEGRO_BITMAP* centro_lousa = NULL;
+ALLEGRO_BITMAP* esquerda_lousa = NULL;
+ALLEGRO_BITMAP* direita_lousa = NULL;
 
-int mapa[11][15] = {        {2,2,2,2,2,7,5,5,5,6,2,2,2,2,2},
+int mapa[11][15] = { {2,2,2,2,2,7,5,5,5,6,2,2,2,2,2},
                                 {2,0,0,1,1,0,1,1,0,0,1,1,0,0,3},
                                 {2,0,4,1,4,1,4,0,4,1,4,1,4,0,2},
                                 {2,1,0,1,1,0,1,0,1,1,0,1,1,1,2},
@@ -27,23 +27,23 @@ int mapa[11][15] = {        {2,2,2,2,2,7,5,5,5,6,2,2,2,2,2},
                                 {2,0,4,1,4,1,4,0,4,1,4,1,4,0,2},
                                 {2,0,0,1,1,0,1,0,1,1,0,1,0,0,2},
                                 {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2}
-                                };  // 0 - chao da sala
-                                    // 1 - prova com nota 0
-                                    // 2 - parede da sala
-                                    // 3 - porta
-                                    // 4 - mesa de aula
-                                    // 5 - meio da lousa
-                                    // 6 - canto direito da lousa
-                                    // 7 - canto esquerdo da lousa
-                                    // 8 - personagem
-                                    // 9 - bomba
-                                    // 10 - explosao
+};  // 0 - chao da sala
+// 1 - prova com nota 0
+// 2 - parede da sala
+// 3 - porta
+// 4 - mesa de aula
+// 5 - meio da lousa
+// 6 - canto direito da lousa
+// 7 - canto esquerdo da lousa
+// 8 - personagem
+// 9 - bomba
+// 10 - explosao
 
-void desenhaMapa(){
+void desenhaMapa() {
 
 }
 
-int main(){
+int main() {
 
 
     al_init();
@@ -51,13 +51,13 @@ int main(){
     al_install_keyboard();
 
 
-    janela = al_create_display(500,500);
-    al_set_window_position(janela, 100,100);
+    janela = al_create_display(500, 500);
+    al_set_window_position(janela, 100, 100);
     al_set_window_title(janela, "BOMBERMAN");
 
     const float FPS = 60;
 
-    ALLEGRO_TIMER *timer = al_create_timer(1/FPS);
+    ALLEGRO_TIMER* timer = al_create_timer(1 / FPS);
 
     fEventos = al_create_event_queue();
     al_register_event_source(fEventos, al_get_display_event_source(janela));
@@ -67,7 +67,7 @@ int main(){
 
     al_start_timer(timer);
     bool done = false;
-    while(!done){
+    while (!done) {
 
 
     }
